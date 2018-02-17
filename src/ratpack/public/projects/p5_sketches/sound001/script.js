161480dcfe8a6eeb25814f93bc0720f09a0c6118
jojo <jojomickymack@gmail.com>
2018-02-17 10:08:01 -0700

    fixing sound sketch, did it the hard way previously

--- src/ratpack/public/projects/p5_sketches/sound001/script.js
+++ src/ratpack/public/projects/p5_sketches/sound001/script.js
@@ -1,21 +1,18 @@
 var song;
 
+var light, dark, current;
 var changing = false;
-var changeSpeed = 5;
-
-var light, dark, current, target;
-var lightString = 'rgb(168, 240, 122)'
-var darkString = 'rgb(68, 53, 91)'
+var lerpCounter = 0.0;
+var lerpInc = -0.01;
 
 function preload() {
     song = loadSound('sound.ogg');
 }
 
 function setup() {
-    light = color(lightString);
-    dark = color(darkString);
+    light = color(168, 240, 122);
+    dark = color(68, 53, 91);
     current = light;
-    target = light;
 
     var myCanvas = createCanvas(windowWidth, windowHeight);
     myCanvas.parent('myCanvas');
@@ -24,46 +21,23 @@ function setup() {
 }
 
 function draw() {
-
     if (changing) {
-        if (red(current) < red(target)) {
-            current.setRed(red(current) + changeSpeed);
-        } else if (red(current) > red(target)) {
-            current.setRed(red(current) - changeSpeed);
-        }
-        if (green(current) < green(target)) {
-            current.setGreen(green(current) + changeSpeed);
-        } else if (green(current) > green(target)) {
-            current.setGreen(green(current) - changeSpeed);
-        }
-        if (blue(current) < blue(target)) {
-            current.setBlue(blue(current) + changeSpeed);
-        } else if (blue(current) > blue(target)) {
-            current.setBlue(blue(current) - changeSpeed);
-        }
-        if (red(current) > red(target) - changeSpeed && (red(current) < red(target) + changeSpeed)
-        && (green(current) > green(target) - changeSpeed) && (green(current) < green(target) + changeSpeed)
-        && (blue(current) > blue(target) - changeSpeed) && (blue(current) < blue(target) + changeSpeed)) {
-            changing = false;
-        }
+        lerpCounter += lerpInc;
+        current = lerpColor(light, dark, lerpCounter);
+        if (lerpCounter >= 1.0 || lerpCounter <= 0.0) changing = false;
     }
 
     background(current)
+    textSize(18);
+    fill(0, 102, 153);
+    text('click on the window to pause/play the sound', 10, 30);
 }
 
 function mousePressed() {
     changing = true;
-    if (red(target) == red(color(lightString))) {
-        target = color(darkString);
-    } else if (red(target) == red(color(darkString))) {
-        target = color(lightString);
-    }
+    lerpInc *= -1;
 
-    if (song.isPlaying()) {
-        song.pause();
-    } else {
-        song.play();
-    }
+    if (song.isPlaying()) song.pause(); else song.play();
 }
 
 function windowResized() {
